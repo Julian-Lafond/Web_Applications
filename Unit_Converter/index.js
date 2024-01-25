d814ab2c6f936c13ca2fcd3db1dd187a30d2ee1f
jmlafond@ucsc.edu <jmlafond@ucsc.edu>
2024-01-24 17:53:44 -0800

    add

--- Unit_Converter/index.js
+++ Unit_Converter/index.js
@@ -3,7 +3,7 @@ let grab_button = document.getElementById("button-el")
 let input_info = document.getElementById("input-el")
 
 
-grab_button.addEventListener('click', function(){
+grab_button.addEventListener('click', function () {
     bodycontainer.innerHTML = `
         <ul>
             <li> Length (Meters/Feet)</li>
@@ -12,9 +12,11 @@ grab_button.addEventListener('click', function(){
                 | ${input_info.value} feet =  ${(input_info.value / 3.281).toFixed(3)} meters
             </p>
             <li>Volume (Liters/Gallons)</li>
-                
+                ${input_info.value} liters = ${(input_info.value * 0.264).toFixed(3)} gallons  
+                | ${input_info.value} gallons =  ${(input_info.value / 0.264).toFixed(3)} liters   
             <li>Mass (Kilograms/Pounds)</li>
+                ${input_info.value} kilos = ${(input_info.value * 2.204).toFixed(3)} pounds  
+|               ${input_info.value} pounds =  ${(input_info.value / 2.204).toFixed(3)} kilos   
         </div>
-    
     `
 })
