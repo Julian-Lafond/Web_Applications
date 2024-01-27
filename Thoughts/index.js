2e0b186e5aa385cc7b874d3882ca3a13aa4b480a
jmlafond@ucsc.edu <jmlafond@ucsc.edu>
2024-01-26 20:03:07 -0800

    set up the delete

--- Thoughts/index.js
+++ Thoughts/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 const appSettings = {
@@ -16,35 +16,46 @@ const input_element = document.getElementById("text-el")
 const button_element = document.getElementById("button-el")
 const peoples_thoughts_page = document.getElementById("ul-el")
 
-let add_thoughts = "";
-button_element.addEventListener('click', function(){
+button_element.addEventListener('click', function () {
     push(thoughtsListInDB, input_element.value)
     clearInput();
 })
 
 
-onValue(thoughtsListInDB, function(snapshot){
-    let thoughtsArray = Object.values(snapshot.val())
-    clearList()
-    for (let i = 0; i < thoughtsArray.length; i++){
-        console.log(thoughtsArray[i]);
-        appendToThoughtsArray(thoughtsArray[i]);
+onValue(thoughtsListInDB, function (snapshot) {
+    if (snapshot.exists()) {
+        let thoughtsArray = Object.entries(snapshot.val())
+        clearList()
+        for (let i = 0; i < thoughtsArray.length; i++) {
+            let currentItem = thoughtsArray[i]
+            appendToThoughtsArray(currentItem);
+        }
     }
-    
+    else {
+        peoples_thoughts_page.innerHTML = "Nothing posted yet..."
+    }
+
 })
 
-function clearList(){
+function clearList() {
     peoples_thoughts_page.innerHTML = " ";
 }
 
-function clearInput(){
+function clearInput() {
     input_element.value = " ";
 }
 
-function appendToThoughtsArray(item){
-    peoples_thoughts_page.innerHTML += `
-        <li>
-            ${item}
-        </li>
-    `
+function appendToThoughtsArray(item) {
+    let itemId = item[0]
+    let itemValue = item[1]
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function () {
+        let exactLocationOfDB = ref(database, `thoughtsList/${itemId}`)
+        remove(exactLocationOfDB);
+    })
+
+    peoples_thoughts_page.append(newEl);
+
 }
