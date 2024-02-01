cabda08b4e118d6ea714d908dad0f35f6cddb743
jmlafond@ucsc.edu <jmlafond@ucsc.edu>
2024-02-01 14:22:44 -0800

    initial setup

--- Twitter_Clone/index.js
+++ Twitter_Clone/index.js
@@ -1,6 +1,10 @@
 const talkBtn = document.getElementById('talk-btn')
+const chatInput = document.getElementById('cha-input')
+
+
 
 talkBtn.addEventListener('click', function(){
-
+    console.log(chatInput.value)
+    chatInput.value = ' '
 })
 
