26667a4368c9709182a3b839aa5e63128ea1b179
jmlafond@ucsc.edu <jmlafond@ucsc.edu>
2024-02-01 19:03:15 -0800

    initial setup

--- Twitter_Clone/index.js
+++ Twitter_Clone/index.js
@@ -2,6 +2,7 @@ import { tweetsData } from './data.js'
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
 
+
 tweetBtn.addEventListener('click', function () {
     console.log(tweetInput.value)
 })
@@ -41,4 +42,9 @@ function getFeedHtml() {
     return feedHtml
 }
 
-console.log(getFeedHtml())
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+
+}
+
+render()
