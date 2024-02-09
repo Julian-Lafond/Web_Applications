5a289c5b2ce559a21cf350f54c049b3a61bc8c70
jmlafond@ucsc.edu <jmlafond@ucsc.edu>
2024-02-09 10:06:04 -0800

    parse through json file

--- Restaurant_Ordering/data.js
+++ Restaurant_Ordering/data.js
@@ -1,23 +1,25 @@
 const menuArray = [
     {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        name: "Shrimp Roll",
+        ingredients: ["Rice", "Shrimp"],
         id: 0,
-        price: 14,
-        emoji: "🍕"
+        price: 12,
+        emoji: "🍤"
     },
     {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
+        name: "Salmon Roll",
+        ingredients: ["Rice", "Salmon", "Cucmber"],
+        price: 10,
+        emoji: "🍣",
         id: 1
     },
         {
-        name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
-        price: 12,
-        emoji: "🍺",
+        name: "Gyoza",
+        ingredients: ["Pork", "Green Onions"],
+        price: 8,
+        emoji: "🥟",
         id: 2
     }
 ]
+
+export default menuArray
---
+++ Restaurant_Ordering/index.js
@@ -0,0 +1,5 @@
+import menuArray from './data.js'
+
+let get_menu = document.getElementById("menu")
+
+console.log(menuArray)
