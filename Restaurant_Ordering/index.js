2cee82fb22d686a704d40457daee301bcac162f5
jmlafond@ucsc.edu <jmlafond@ucsc.edu>
2024-02-11 10:06:28 -0800

    changed ui

--- Restaurant_Ordering/index.js
+++ Restaurant_Ordering/index.js
@@ -3,21 +3,31 @@ import menuArray from './data.js'
 let get_menu = document.getElementById("menu")
 
 
+function getIngredients(){
+    ingredientsArray = []
+    for (let item of menuArray){
+        for (let ingredients of item.ingredients){
+            ingredientsArray.push(ingredients)
+        }
+    }
+    return ingredientsArray
+}
+
 function getData(menuArray) {
     let menuItems = ""
+    let ingredientsArray = getIngredients()
     for (let item of menuArray) {
-        menuItems += `
+            menuItems += `
             <div class = "container">
                 <p class = "emoji">${item.emoji}</p>
                 <div class = "menuItems">
                     <h1>${item.name}</h1>
-                    <h3>${item.ingredients}</h3>
                     <h2>$${item.price}</h2>
                     <hr>
                 </div>
             </div>
         `
-    }
+        }
     get_menu.innerHTML = menuItems
 }
 
