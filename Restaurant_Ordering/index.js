07fbc1284bdb372571be61ecb2c49783c09a3741
jmlafond@ucsc.edu <jmlafond@ucsc.edu>
2024-02-10 17:57:35 -0800

    parsing json file

--- Restaurant_Ordering/index.js
+++ Restaurant_Ordering/index.js
@@ -2,4 +2,24 @@ import menuArray from './data.js'
 
 let get_menu = document.getElementById("menu")
 
-console.log(menuArray)
+
+function getData(menuArray) {
+    let menuItems = ""
+    for (let item of menuArray) {
+        menuItems += `
+            <div class = "menuItems">
+                <p>${item.emoji}</p>
+                <h1>${item.name}</h1>
+                <h3>${item.ingredients}</h3>
+                <h2>${item.price}</h2>
+
+            </div>
+        
+        `
+    }
+    get_menu.innerHTML = menuItems
+}
+
+getData(menuArray)
+
+
